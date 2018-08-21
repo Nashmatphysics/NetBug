1f7a2c78f3af9bd19ccef0537ce0d884b91ed789
nash <nashemt@outlook.com>
2018-08-22 07:46:14 +0800

    creat_object

---
+++ NetBug/NetBug0.1/main.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <windows.h>
+#include <time.h>
+#include <stdarg.h>
+
+int main(void)
+{
+
+//getchar();
+system("pause");
+return 0;
+}
